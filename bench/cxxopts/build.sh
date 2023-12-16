766fecb71236dc97e95ce0f262580a98f7d9750d
Yingwei Zheng <dtcxzyw2333@gmail.com>
2023-12-17 00:01:00 +0800

    bench: Add cxxopts

---
+++ bench/cxxopts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p bench_build
+cd bench_build
+../../../scripts/configure_cmake.sh ../cxxopts -DCXXOPTS_BUILD_TESTS=OFF
+cmake --build . -j
+cd ..
+find bench_build/src -name "*.o" -exec ../../scripts/extract_bc.sh {} \;
