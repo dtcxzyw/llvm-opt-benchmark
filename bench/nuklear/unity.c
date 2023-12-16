5da5f8705e16db43e50bdb27354f48d8a8359b7a
Yingwei Zheng <dtcxzyw2333@gmail.com>
2023-12-17 00:14:51 +0800

    bench: Add nuklear

---
+++ bench/nuklear/unity.c
@@ -0,0 +1,19 @@
+/* nuklear - v1.05 - public domain */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <limits.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#include "nuklear.h"
