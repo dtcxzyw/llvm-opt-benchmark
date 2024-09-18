897d12c3bd6c4b135a064b2e68dd8a1a7b43c740
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-09-18 19:56:01 +0800

    script: Filter out small diffs

--- scripts/stats_diff.py
+++ scripts/stats_diff.py
@@ -3,6 +3,7 @@ import sys
 import os
 import json
 
+topk = 10
 v1_file = sys.argv[1]
 v2_file = sys.argv[2]
 
@@ -15,15 +16,30 @@ v2 = json.load(open(v2_file))
 if v1 == v2:
     exit(0)
 
-print('\n```')
+improvements = []
+regressions = []
+
 for k in v1:
     if k in v2:
         origin = v1[k]
         new = v2[k]
-        if origin != new:
-            if origin == 0:
-                diff = 0
+        if origin != new and origin != 0:
+            diff = (new - origin) / origin * 100.0
+            if diff > 0.0:
+                improvements.append((k, origin, new, diff))
             else:
-                diff = (new - origin) / origin * 100.0
-            print(f'{k} {origin} -> {new} {diff:+.2f}%')
+                regressions.append((k, origin, new, diff))
+
+improvements.sort(key=lambda x: x[3], reverse=True)
+regressions.sort(key=lambda x: x[3])
+improvements = improvements[:topk]
+regressions = regressions[:topk]
+
+print('\n```')
+print('Improvements:')
+for k, origin, new, diff in improvements:
+    print(f'{k} {origin} -> {new} {diff:+.2f}%')
+print('Regressions:')
+for k, origin, new, diff in regressions:
+    print(f'{k} {origin} -> {new} {diff:+.2f}%')
 print('```')
