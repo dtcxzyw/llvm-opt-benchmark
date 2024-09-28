c4471e70295ffaddd372e3482cfd7a3544e63232
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-09-28 17:08:26 +0800

    script: count compilation time by projects

--- scripts/comptime_diff.py
+++ scripts/comptime_diff.py
@@ -24,52 +24,67 @@ def load(filename):
 v1 = load(v1_file)
 v2 = load(v2_file)
 
-regression = []
-improvement = []
+def compare(v1, v2, type):
+    regression = []
+    improvement = []
 
-ratio_sum = 0.0
-cnt = 0
+    ratio_sum = 0.0
+    cnt = 0
 
+    for k in v1:
+        if k not in v2:
+            continue
+        t1 = v1[k]
+        t2 = v2[k]
+        if t1 <= 0 or t2 <= 0:
+            continue
+        
+        ratio_sum += math.log(t2/t1)
+        cnt += 1
+
+        diff = abs(t1 - t2) / t1
+        if diff > threshold:
+            if t1 < t2:
+                regression.append((k, t1, t2))
+            else:
+                improvement.append((k, t1, t2))
+
+    regression.sort(key=lambda x: (x[2] - x[1])/x[1], reverse=True)
+    improvement.sort(key=lambda x: (x[2] - x[1])/x[1])
+
+    regression = regression[:topk]
+    improvement = improvement[:topk]
+
+    ratio_geomean = (math.exp(ratio_sum / cnt) - 1.0) * 100.0
+
+    if len(regression) == 0 and len(improvement) == 0 and abs(ratio_geomean) < 0.001:
+        return 0
+
+    print("\n```")
+    print(f"Compilation time result (by {type}):")
+    print(f"Top {topk} improvements:")
+    for k, t1, t2 in improvement:
+        diff = "{:+.2f}%".format((t2 - t1)/t1*100.0)
+        print(f"  {k} {t1} -> {t2} {diff}")
+
+    print(f"Top {topk} regressions:")
+    for k, t1, t2 in regression:
+        diff = "{:+.2f}%".format((t2 - t1)/t1*100.0)
+        print(f"  {k} {t1} -> {t2} {diff}")
+
+    print(f"\nOverall: {ratio_geomean:.8f}%")
+    print("```")
+    return 0 if ratio_geomean < 0.1 else 1
+
+err = compare(v1, v2, "files")
+p1 = dict()
+p2 = dict()
 for k in v1:
     if k not in v2:
         continue
-    t1 = v1[k]
-    t2 = v2[k]
-    if t1 <= 0 or t2 <= 0:
-        continue
-    
-    ratio_sum += math.log(t2/t1)
-    cnt += 1
+    proj = k[:k.find("/")]
+    p1[proj] = p1.get(proj, 0) + v1[k]
+    p2[proj] = p2.get(proj, 0) + v2[k]
 
-    diff = abs(t1 - t2) / t1
-    if diff > threshold:
-        if t1 < t2:
-            regression.append((k, t1, t2))
-        else:
-            improvement.append((k, t1, t2))
-
-regression.sort(key=lambda x: (x[2] - x[1])/x[1], reverse=True)
-improvement.sort(key=lambda x: (x[2] - x[1])/x[1])
-
-regression = regression[:topk]
-improvement = improvement[:topk]
-
-ratio_geomean = (math.exp(ratio_sum / cnt) - 1.0) * 100.0
-
-if len(regression) == 0 and len(improvement) == 0 and abs(ratio_geomean) < 0.001:
-    exit(0)
-
-print("\n```")
-print(f"Top {topk} improvements:")
-for k, t1, t2 in improvement:
-    diff = "{:+.2f}%".format((t2 - t1)/t1*100.0)
-    print(f"  {k} {t1} {t2} {diff}")
-
-print(f"Top {topk} regressions:")
-for k, t1, t2 in regression:
-    diff = "{:+.2f}%".format((t2 - t1)/t1*100.0)
-    print(f"  {k} {t1} {t2} {diff}")
-
-print(f"\nOverall: {ratio_geomean:.8f}%")
-print("```")
-exit(0 if ratio_geomean < 0.1 else 1)
+err += compare(p1, p2, "projects")
+exit(1 if err > 0 else 0)
