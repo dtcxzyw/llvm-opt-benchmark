f1a54a709432104e86107b1130f27c1e19dc789d
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-10-02 22:38:53 +0800

    ci: support multi-line commands

--- scripts/github_rest.py
+++ scripts/github_rest.py
@@ -4,22 +4,24 @@ import os
 import subprocess
 from authorized_users import authorized_users
 
-cmd = os.getenv('GH_COMMAND').strip()
-if not cmd.startswith('/'):
-    exit(0)
 actor = os.getenv('GH_ACTOR')
 if actor not in authorized_users:
     exit(0)
 number = os.getenv('GH_PR_NUMBER')
 
-if cmd == '/close':
-    subprocess.check_call(['gh', 'pr', 'close', number])
-elif cmd == '/reopen':
-    subprocess.check_call(['gh', 'pr', 'reopen', number])
-else:
-    if cmd.startswith('/add-label '):
-        label = cmd.removeprefix('/add-label ')
-        subprocess.check_call(['gh', 'pr', 'edit', number, '--add-label', label])
-    elif cmd.startswith('/remove-label '):
-        label = cmd.removeprefix('/remove-label ')
-        subprocess.check_call(['gh', 'pr', 'edit', number, '--remove-label', label])
+cmd = os.getenv('GH_COMMAND')
+for line in cmd.splitlines():
+    line = line.strip()
+    if not line.startswith('/'):
+        continue
+    if line == '/close':
+        subprocess.check_call(['gh', 'pr', 'close', number])
+    elif line == '/reopen':
+        subprocess.check_call(['gh', 'pr', 'reopen', number])
+    else:
+        if line.startswith('/add-label '):
+            label = line.removeprefix('/add-label ')
+            subprocess.check_call(['gh', 'pr', 'edit', number, '--add-label', label])
+        elif line.startswith('/remove-label '):
+            label = line.removeprefix('/remove-label ')
+            subprocess.check_call(['gh', 'pr', 'edit', number, '--remove-label', label])
