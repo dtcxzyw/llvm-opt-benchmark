efc98a9794360293b98140750b6cff36dffa26ff
Yingwei Zheng <dtcxzyw2333@gmail.com>
2024-10-23 12:00:57 +0800

    ci: Use lld for cov

--- scripts/build_cov.sh
+++ scripts/build_cov.sh
@@ -7,6 +7,6 @@ cd llvm/llvm-cov-build
 cmake ../llvm-project/llvm -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -G Ninja \
     -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_ENABLE_WARNINGS=OFF -DLLVM_APPEND_VC_REV=OFF -DLLVM_TARGETS_TO_BUILD="X86;" \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DLLVM_BUILD_INSTRUMENTED_COVERAGE=ON \
-    -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
+    -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_USE_LINKER=lld
 cmake --build . -j -t opt
 cmake --build . -j -t clear-profile-data
