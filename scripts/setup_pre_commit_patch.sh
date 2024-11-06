5db7e369df8bd193aad5954a7385d0d749de4e92
michaelmaitland <6876996+michaelmaitland@users.noreply.github.com>
2024-11-06 10:43:22 +0800

    pre-commit: PRmichaelmaitland/llvm-project/commit/94ff86ef997cf2cce9c58c02c88d27056b68859e

--- scripts/setup_pre_commit_patch.sh
+++ scripts/setup_pre_commit_patch.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+export GITHUB_PATCH_ID=michaelmaitland/llvm-project/commit/94ff86ef997cf2cce9c58c02c88d27056b68859e
 
 # Please rebase manually
 # git fetch origin
