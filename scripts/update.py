8f4da4e0e16f8f1ba3fd2fef636ea66e25c5e360
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-02-01 18:55:33 +0800

    script: Increase timeout

--- scripts/update.py
+++ scripts/update.py
@@ -398,7 +398,7 @@ def update_pr():
     lines.append(run_cmd("head -100 test.log"))
 
     if not NO_DIFF:
-        out = run_cmd("git diff {} --numstat --oneline bench/*".format(base))
+        out = run_cmd("git diff {} --numstat --oneline bench/*".format(base), to=1800.0)
         if out is not None:
             out = out.split("\n")
             out = out[0:min(len(out), 200)]
