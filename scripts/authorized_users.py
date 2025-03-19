29fcfa15e93639a46218fa2ddb912f0c9b794bb2
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-03-19 15:07:23 +0800

    scripts: Fix authorization

--- scripts/authorized_users.py
+++ scripts/authorized_users.py
@@ -1,20 +1,19 @@
-import subprocess
-
+import requests
+import os
 
 def is_authorized_users(user):
     try:
-        subprocess.check_call(
-            [
-                "gh",
-                "api",
-                "-H",
-                "Accept: application/vnd.github+json",
-                "-H",
-                "X-GitHub-Api-Version: 2022-11-28",
-                f"/orgs/llvm/members/{user}",
-            ]
+        github_token = os.environ["GITHUB_TOKEN_LLVM"]
+        session = requests.Session()
+        session.headers.update(
+            {
+                "X-GitHub-Api-Version": "2022-11-28",
+                "Authorization": f"Bearer {github_token}",
+                "Accept": "application/vnd.github+json",
+            }
         )
-        return True
+        res = session.get(f"https://api.github.com/orgs/llvm/members/{user}", timeout=120)
+        return res.status_code == 204
     except Exception as e:
         print(e)
         return False
