e5c4e6c9ecf1b499bd8f1c9e626f4049301cf18a
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-08-02 20:38:02 +0800

    bench: remove trivial files

--- scripts/remove_trivial.py
+++ scripts/remove_trivial.py
@@ -9,25 +9,32 @@ def is_trivial(file):
     if os.path.getsize(file) > max_size:
         return True
 
-    with open(file) as f:
-        for line in f.readlines():
-            if line.startswith("define "):
-                return False
-    return True
+    return False
+    # with open(file) as f:
+    #     for line in f.readlines():
+    #         if line.startswith("define "):
+    #             return False
+    # return True
 
 for dir in os.listdir(bench_dir):
     optimized = os.path.join(bench_dir, dir, "optimized")
     if not os.path.exists(optimized):
         continue
+    print(dir)
 
     for file in os.listdir(optimized):
         full_path = os.path.join(optimized, file)
-        if is_trivial(full_path):
-            original_path = full_path.replace("/optimized/", "/original/")
-            if os.path.exists(original_path):
-                print(full_path)
-                os.remove(full_path)
-                os.remove(original_path)
-                removed += 1
+        original_path = full_path.replace("/optimized/", "/original/")
+        if not os.path.exists(original_path):
+            print(full_path)
+            os.remove(full_path)
+            removed += 1
+            continue
+
+        if is_trivial(full_path) or is_trivial(original_path):
+            print(full_path)
+            os.remove(full_path)
+            os.remove(original_path)
+            removed += 1
 
 print("Removed: ", removed)
