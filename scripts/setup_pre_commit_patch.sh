7b8ce901623506b0f02adfc5e7d658b229e9f710
nikic <216080+nikic@users.noreply.github.com>
2025-08-19 20:50:15 +0800

    pre-commit: PRnikic/llvm-project/commit/f4c1e5e950ce172a5e0e02be65602229c951ddc6

--- scripts/setup_pre_commit_patch.sh
+++ scripts/setup_pre_commit_patch.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+export GITHUB_PATCH_ID=nikic/llvm-project/commit/f4c1e5e950ce172a5e0e02be65602229c951ddc6
 export COMPTIME_MODE=0
 
 # Please rebase manually
