25f8f14fa604079a8c9d26d1319a8d27443eb32f
fhahn <450489+fhahn@users.noreply.github.com>
2025-09-25 17:23:59 +0800

    pre-commit: PRfhahn/llvm-project/tree/ic-remove-assume-align-1

--- scripts/setup_pre_commit_patch.sh
+++ scripts/setup_pre_commit_patch.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+export GITHUB_PATCH_ID=fhahn/llvm-project/tree/ic-remove-assume-align-1
 export COMPTIME_MODE=0
 export STAT_MODE=0
 export STAT_NAME=""
