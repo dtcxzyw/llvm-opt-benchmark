a761c22988ee7aefffc84b0bfbe6223cbd312665
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-10-07 02:20:05 +0800

    script: reset and clean llvm/llvm-project
    Signed-off-by: Yingwei Zheng <dtcxzyw2333@gmail.com>

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -2,6 +2,7 @@ git config user.name "github-actions[bot]"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 if [ -f llvm/llvm-project/.git ];
 then
+git -C llvm/llvm-project reset HEAD
 git -C llvm/llvm-project checkout .
 git -C llvm/llvm-project clean -fdx
 fi
