7e745dfdddae8aad2644f38f4e6275af9512d643
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-10-09 22:02:56 +0800

    bench: remove dead tests

--- scripts/remove_dead.py
+++ scripts/remove_dead.py
@@ -20,15 +20,16 @@ for dir in os.listdir(bench_dir):
         full_path = os.path.join(optimized, file)
         tasks.append(full_path)
 
-for full_path in tqdm(tasks):
+prog = tqdm(tasks)
+for full_path in prog:
     last_time = subprocess.check_output(["git", "log", "-1", "--pretty=%ad", full_path]).decode("utf-8").strip()
     last_time = datetime.datetime.strptime(last_time, "%a %b %d %H:%M:%S %Y %z")
     if last_time < last_active_time:
         original_path = full_path.replace("/optimized/", "/original/")
         if os.path.exists(original_path):
-            print(full_path, last_time)
+            prog.write(full_path + " " + str(last_time))
             os.remove(full_path)
             os.remove(original_path)
             removed += 1
-
+prog.close()
 print("Removed: ", removed)
