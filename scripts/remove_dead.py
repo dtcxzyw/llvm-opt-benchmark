7765be8fb6fbdc21b7913298ad3916b81ab60cda
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-10-09 20:26:59 +0800

    script: add progress bar

--- scripts/remove_dead.py
+++ scripts/remove_dead.py
@@ -2,6 +2,7 @@ import sys
 import os
 import subprocess
 import datetime
+from tqdm import tqdm
 
 bench_dir = sys.argv[1]
 time_to_live = 180
@@ -9,7 +10,7 @@ current_time = datetime.datetime.now(datetime.timezone.utc)
 last_active_time = current_time - datetime.timedelta(days=time_to_live)
 print("Last active time: ", last_active_time)
 removed = 0
-
+tasks = []
 for dir in os.listdir(bench_dir):
     optimized = os.path.join(bench_dir, dir, "optimized")
     if not os.path.exists(optimized):
@@ -17,14 +18,17 @@ for dir in os.listdir(bench_dir):
 
     for file in os.listdir(optimized):
         full_path = os.path.join(optimized, file)
-        last_time = subprocess.check_output(["git", "log", "-1", "--pretty=%ad", full_path]).decode("utf-8").strip()
-        last_time = datetime.datetime.strptime(last_time, "%a %b %d %H:%M:%S %Y %z")
-        if last_time < last_active_time:
-            original_path = full_path.replace("/optimized/", "/original/")
-            if os.path.exists(original_path):
-                print(full_path, last_time)
-                os.remove(full_path)
-                os.remove(original_path)
-                removed += 1
+        tasks.append(full_path)
+
+for full_path in tqdm(tasks):
+    last_time = subprocess.check_output(["git", "log", "-1", "--pretty=%ad", full_path]).decode("utf-8").strip()
+    last_time = datetime.datetime.strptime(last_time, "%a %b %d %H:%M:%S %Y %z")
+    if last_time < last_active_time:
+        original_path = full_path.replace("/optimized/", "/original/")
+        if os.path.exists(original_path):
+            print(full_path, last_time)
+            os.remove(full_path)
+            os.remove(original_path)
+            removed += 1
 
 print("Removed: ", removed)
