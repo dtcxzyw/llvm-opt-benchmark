fe410229d6a4459d61ad0c5c4962519f78741e3c
andjo403 <844398+andjo403@users.noreply.github.com>
2026-03-10 06:04:57 +0800

    pre-commit: PR185524

--- scripts/setup_pre_commit_patch.sh
+++ scripts/setup_pre_commit_patch.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
+export GITHUB_PATCH_ID=llvm/llvm-project/pull/185524
 export COMPTIME_MODE=0
 export STAT_MODE=0
 export STAT_NAME=""
