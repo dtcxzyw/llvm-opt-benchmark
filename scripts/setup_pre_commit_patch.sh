d72ee39735f5726a314b6b68c2a352a74dc30fd2
Camsyn <65994555+Camsyn@users.noreply.github.com>
2026-03-11 17:52:35 +0800

    pre-commit: PR185844

--- scripts/setup_pre_commit_patch.sh
+++ scripts/setup_pre_commit_patch.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-export GITHUB_PATCH_ID="<user_name>/llvm-project/commit/<commit_hash>"
-export COMPTIME_MODE=0
+export GITHUB_PATCH_ID=llvm/llvm-project/pull/185844
+export COMPTIME_MODE=1
 export STAT_MODE=0
 export STAT_NAME=""
 
